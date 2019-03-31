eb9851881040871a788d0aa8f00a0fb45e23749e
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro.local>
2019-03-31 06:02:07 -0700

    made side panel options transition and scrollable

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-const app = () => {
-	let sidePanelOptions = document.querySelectorAll('.side-panel-option');
-	let sidePanelOptionSelector = document.querySelector('.side-panel-option-selector');
-	console.log(sidePanelOptions);
-	sidePanelOptions.forEach(sidePanelOption => {
-		console.log(sidePanelOption);
-		sidePanelOption.addEventListener('click', (event) => {
-			let currentIndex = parseInt(event.target.getAttribute('index'));
-			let sidePanelOptionSelectorStyle = window.getComputedStyle(sidePanelOptionSelector);
-			sidePanelOptionSelector.style.top = `${currentIndex * parseInt(sidePanelOptionSelectorStyle.height)}px`;
-		});
-	});
-}
+window.onload = () => {
+  const app = () => {
+    let sidePanelOptions = document.querySelectorAll(".side-panel-option");
+    let sidePanelOptionSelectorManager = new SidePanelOptionSelectorManager();
 
-app();
+    sidePanelOptions.forEach(sidePanelOption => {
+      sidePanelOption.addEventListener("click", event => {
+        let currentIndex = parseInt(event.target.getAttribute("index"));
+        sidePanelOptionSelectorManager.highlight(currentIndex);
+      });
+    });
+  };
+
+  class SidePanelOptionSelectorManager {
+    highlight(index) {
+      let self = document.querySelector(".side-panel-option-selector-manager");
+      let topCover = document.querySelector(
+        ".side-panel-option-selector-manager div:nth-child(1)"
+      );
+      let highlighter = document.querySelector(
+        ".side-panel-option-selector-manager div:nth-child(2)"
+      );
+      let bottomCover = document.querySelector(
+        ".side-panel-option-selector-manager div:nth-child(3)"
+      );
+
+      let selfStyle = window.getComputedStyle(self);
+      let highlighterStyle = window.getComputedStyle(highlighter);
+      console.log(index);
+      highlighter.style.top = `${index * parseInt(highlighterStyle.height)}px`;
+      console.log(highlighter.style.top);
+      topCover.style.height = `${parseInt(highlighter.style.top)}px`;
+      bottomCover.style.height = `${parseInt(selfStyle.height) -
+        parseInt(topCover.style.height) -
+        parseInt(highlighterStyle.height)}px`;
+    }
+  }
+
+  app();
+
+  let sidePanelOptionSelectorManager = new SidePanelOptionSelectorManager();
+  sidePanelOptionSelectorManager.highlight(0);
+};
